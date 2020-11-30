5fb77a8ed157c0635f2f28865f427bae5cd81c80
Gerson <gerson.lopezatolica.edu.sv>
2020-11-30 15:27:16 -0600

    Primer commit

---
+++ app/src/main/java/sv/edu/catolica/MainActivity.java
@@ -0,0 +1,197 @@
+package sv.edu.catolica;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.security.cert.X509Certificate;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST =1 ;
+    private EditText t1,t2,t3;
+    private Button buscar, enviar;
+    ImageView img;
+    Bitmap bitmap;
+    String encodeImageString;
+
+    private static final String url="http://192.168.1.9/rapiNotis/fileupload.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        img=(ImageView)findViewById(R.id.img);
+        enviar=(Button)findViewById(R.id.btnSubir);
+        buscar=(Button)findViewById(R.id.btnBuscar);
+
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withActivity(MainActivity.this)
+                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                                Intent intent =new Intent(Intent.ACTION_PICK);
+                                intent.setType("image/*");
+                                intent.setAction(Intent.ACTION_GET_CONTENT);
+                                startActivityForResult(Intent.createChooser(intent,"Buscar Imagen"),1);
+
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                permissionToken.cancelPermissionRequest();
+
+                            }
+                        }).check();
+
+            }
+        });
+
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                cargardatosdb();
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode==1 && resultCode==RESULT_OK)
+        {
+            Uri filepath=data.getData();
+            try
+            {
+                InputStream inputStream=getContentResolver().openInputStream(filepath);
+                bitmap= BitmapFactory.decodeStream(inputStream);
+                img.setImageBitmap(bitmap);
+                encodeBitmapImage(bitmap);
+
+            }catch (Exception e)
+            {
+
+            }
+
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+
+    private void encodeBitmapImage(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+
+        byte[] bytesofimage=byteArrayOutputStream.toByteArray();
+        encodeImageString=android.util.Base64.encodeToString(bytesofimage, Base64.DEFAULT);
+    }
+
+    private void cargardatosdb()
+    {
+
+        t1=(EditText)findViewById(R.id.t1);
+        t2=(EditText)findViewById(R.id.t2);
+        t3=(EditText)findViewById(R.id.t3);
+
+        final String tipo=t1.getText().toString().trim();
+        final String titulo=t2.getText().toString().trim();
+        final String noticia=t3.getText().toString().trim();
+        
+
+        StringRequest request=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                t1.setText("");
+                t2.setText("");
+                t3.setText("");
+                img.setImageResource(R.drawable.ic_launcher_foreground);
+                Toast.makeText(getApplicationContext(),response.toString(),Toast.LENGTH_LONG).show();
+
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
+
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map=new HashMap<String, String>();
+                map.put("t1",tipo);
+                map.put("t2",titulo);
+                map.put("t3",noticia);
+                map.put("upload",encodeImageString);
+
+
+                return map;
+            }
+        };
+
+
+
+        RequestQueue queue= Volley.newRequestQueue(getApplicationContext());
+        queue.add(request);
+
+    }
+
+
+
+}
